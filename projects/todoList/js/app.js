fad1c897072ccc0aa7d0df4b14fe6f8335fe5342
Nasratullah Talash <ntalash28@gmail.com>
2021-04-14 16:13:09 +0530

    added submit button to form

--- projects/todoList/js/app.js
+++ projects/todoList/js/app.js
@@ -1,16 +1,16 @@
 "use strict";
 const UI = {
-    themeBtn: document.getElementById("theme-btn"),
-    todoInputForm: document.querySelector(".input__form"),
-    todoInput: document.querySelector(".header__input"),
-    todoItemsContainer: document.querySelector(".todo-list"),
-    allBtn: document.querySelector(".btnAll"),
-    activeBtn: document.querySelector(".btnActive"),
-    completedBtn: document.querySelector(".btnCompleted"),
-    filterBtns: document.querySelectorAll(".btnFilter"),
-    activeTasksCount: document.querySelector(".todoItems__number"),
-    clearCompletedBtn: document.querySelector(".clearCompleted-btn"),
-    taskRemoveBtn: document.querySelector(".todo-item__remove-btn"),
+  themeBtn: document.getElementById("theme-btn"),
+  todoInputForm: document.querySelector(".input__form"),
+  todoInput: document.querySelector(".header__input"),
+  todoItemsContainer: document.querySelector(".todo-list"),
+  allBtn: document.querySelector(".btnAll"),
+  activeBtn: document.querySelector(".btnActive"),
+  completedBtn: document.querySelector(".btnCompleted"),
+  filterBtns: document.querySelectorAll(".btnFilter"),
+  activeTasksCount: document.querySelector(".todoItems__number"),
+  clearCompletedBtn: document.querySelector(".clearCompleted-btn"),
+  taskRemoveBtn: document.querySelector(".todo-item__remove-btn"),
 };
 
 // ======== App Data ==========
@@ -20,16 +20,16 @@ let dragSrcEl;
 const themeStyleSheet = document.getElementById("themeStyleSheet");
 
 function displayTasksCount() {
-    const tasksCount = localStorage.getItem("activeTasksCount");
-    if (tasksCount) {
-        UI.activeTasksCount.textContent = tasksCount;
-    }
+  const tasksCount = localStorage.getItem("activeTasksCount");
+  if (tasksCount) {
+    UI.activeTasksCount.textContent = tasksCount;
+  }
 }
 
 function generateTaskMarkup(id, desc, isCompleted = "false") {
-    return `
+  return `
       <li class="todo-item ${
-          isCompleted === "true" ? "done" : ""
+        isCompleted === "true" ? "done" : ""
       }" draggable="true" id="${id}" 
       data-complete="${isCompleted}">
          <input
@@ -46,259 +46,258 @@ function generateTaskMarkup(id, desc, isCompleted = "false") {
    `;
 }
 (function () {
-    if (data.length === 0) return;
+  if (data.length === 0) return;
 
-    UI.todoItemsContainer.innerHTML = "";
-    data.forEach((task) => {
-        const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
-        UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
-    });
-    displayTasksCount();
+  UI.todoItemsContainer.innerHTML = "";
+  data.forEach((task) => {
+    const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
+    UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
+  });
+  displayTasksCount();
 })();
 
 function storeTasks() {
-    localStorage.setItem("tasksData", JSON.stringify(data));
-    const activeTasksCount = data.filter((task) => task.isCompleted === "false")
-        .length;
-    localStorage.setItem("activeTasksCount", activeTasksCount);
+  localStorage.setItem("tasksData", JSON.stringify(data));
+  const activeTasksCount = data.filter((task) => task.isCompleted === "false")
+    .length;
+  localStorage.setItem("activeTasksCount", activeTasksCount);
 }
 
 function dragStartHandler(ev) {
-    let sourceItem = ev.target.closest(".todo-item");
-    const theme = localStorage.getItem("prevSetTheme");
-    sourceItem.style.background = theme == "dark" ? "#212336" : "#e6e6e6";
+  let sourceItem = ev.target.closest(".todo-item");
+  const theme = localStorage.getItem("prevSetTheme");
+  sourceItem.style.background = theme == "dark" ? "#212336" : "#e6e6e6";
 
-    ev.dataTransfer.setData("itemId", sourceItem.id);
-    ev.dataTransfer.effectAllowed = "move";
-    ev.dataTransfer.setData("text/html", sourceItem.innerHTML);
-    dragSrcEl = sourceItem;
+  ev.dataTransfer.setData("itemId", sourceItem.id);
+  ev.dataTransfer.effectAllowed = "move";
+  ev.dataTransfer.setData("text/html", sourceItem.innerHTML);
+  dragSrcEl = sourceItem;
 }
 
 function dragOverHandler(ev) {
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = "move";
-    let targetItem = ev.target.closest(".todo-item");
-    const theme = localStorage.getItem("prevSetTheme");
-    targetItem.style.background = theme == "dark" ? "#212336" : "#e6e6e6";
-    return false;
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+  let targetItem = ev.target.closest(".todo-item");
+  const theme = localStorage.getItem("prevSetTheme");
+  targetItem.style.background = theme == "dark" ? "#212336" : "#e6e6e6";
+  return false;
 }
 
 function dragLeaveHandler(ev) {
-    let targetItem = ev.target.closest(".todo-item");
-    if (targetItem === dragSrcEl) return;
-    targetItem.style.background = "";
+  let targetItem = ev.target.closest(".todo-item");
+  if (targetItem === dragSrcEl) return;
+  targetItem.style.background = "";
 }
 
 function dropHandler(ev) {
-    let targetItem = ev.target.closest(".todo-item");
-    let id = ev.dataTransfer.getData("itemId");
-    let sourceItem = document.getElementById(id);
-    // targetItem.parentElement.insertBefore(sourceItem, targetItem.nextSibling);
+  let targetItem = ev.target.closest(".todo-item");
+  let id = ev.dataTransfer.getData("itemId");
+  let sourceItem = document.getElementById(id);
+  // targetItem.parentElement.insertBefore(sourceItem, targetItem.nextSibling);
 
-    if (dragSrcEl.innerHTML !== targetItem.innerHTML) {
-        const srcAttributes = [...sourceItem.attributes].map((attr) => ({
-            name: attr.name,
-            value: attr.value,
-        }));
-        const targetAttributes = [...targetItem.attributes].map((attr) => ({
-            name: attr.name,
-            value: attr.value,
-        }));
+  if (dragSrcEl.innerHTML !== targetItem.innerHTML) {
+    const srcAttributes = [...sourceItem.attributes].map((attr) => ({
+      name: attr.name,
+      value: attr.value,
+    }));
+    const targetAttributes = [...targetItem.attributes].map((attr) => ({
+      name: attr.name,
+      value: attr.value,
+    }));
 
-        const srcTargetIndices = [];
-        data.filter((task, index) => {
-            if (
-                task.id === Number(targetItem.id) ||
-                task.id === Number(dragSrcEl.id)
-            ) {
-                srcTargetIndices.push(index);
-                return true;
-            }
-        });
-        // Swapping tasks in data array
-        [data[srcTargetIndices[0]], data[srcTargetIndices[1]]] = [
-            data[srcTargetIndices[1]],
-            data[srcTargetIndices[0]],
-        ];
+    const srcTargetIndices = [];
+    data.filter((task, index) => {
+      if (
+        task.id === Number(targetItem.id) ||
+        task.id === Number(dragSrcEl.id)
+      ) {
+        srcTargetIndices.push(index);
+        return true;
+      }
+    });
+    // Swapping tasks in data array
+    [data[srcTargetIndices[0]], data[srcTargetIndices[1]]] = [
+      data[srcTargetIndices[1]],
+      data[srcTargetIndices[0]],
+    ];
 
-        dragSrcEl.innerHTML = targetItem.innerHTML;
-        targetItem.innerHTML = ev.dataTransfer.getData("text/html");
+    dragSrcEl.innerHTML = targetItem.innerHTML;
+    targetItem.innerHTML = ev.dataTransfer.getData("text/html");
 
-        // Swap the attributes of tasks
-        targetAttributes.forEach((attr) =>
-            sourceItem.setAttribute(attr.name, attr.value)
-        );
-        srcAttributes.forEach((attr) =>
-            targetItem.setAttribute(attr.name, attr.value)
-        );
-        console.log(srcAttributes, targetAttributes);
-    }
-    targetItem.style.background = "";
-    sourceItem.style.background = "";
-    storeTasks();
+    // Swap the attributes of tasks
+    targetAttributes.forEach((attr) =>
+      sourceItem.setAttribute(attr.name, attr.value)
+    );
+    srcAttributes.forEach((attr) =>
+      targetItem.setAttribute(attr.name, attr.value)
+    );
+    console.log(srcAttributes, targetAttributes);
+  }
+  targetItem.style.background = "";
+  sourceItem.style.background = "";
+  storeTasks();
 }
 
 const dragDropHandlers = [
-    dragStartHandler,
-    dragOverHandler,
-    dragLeaveHandler,
-    dropHandler,
+  dragStartHandler,
+  dragOverHandler,
+  dragLeaveHandler,
+  dropHandler,
 ];
 
 ["dragstart", "dragover", "dragleave", "drop"].forEach((event, ind) => {
-    UI.todoItemsContainer.addEventListener(event, dragDropHandlers[ind]);
+  UI.todoItemsContainer.addEventListener(event, dragDropHandlers[ind]);
 });
 
 UI.themeBtn.addEventListener("click", (e) => {
-    const themeName = e.target.dataset.theme;
-    themeStyleSheet.href = `./css/${themeName}Theme.css`;
-    UI.themeBtn.dataset.theme = themeName === "light" ? "dark" : "light";
-    localStorage.setItem("prevSetTheme", themeName);
+  const themeName = e.target.dataset.theme;
+  themeStyleSheet.href = `./css/${themeName}Theme.css`;
+  UI.themeBtn.dataset.theme = themeName === "light" ? "dark" : "light";
+  localStorage.setItem("prevSetTheme", themeName);
 });
 
 UI.todoInputForm.addEventListener("submit", (ev) => {
-    ev.preventDefault();
-    const task = {
-        id: new Date().getTime(),
-        desc: UI.todoInput.value,
-        isCompleted: "false",
-    };
+  ev.preventDefault();
+  const task = {
+    id: new Date().getTime(),
+    desc: UI.todoInput.value,
+    isCompleted: "false",
+  };
 
-    if (task.desc === "") return;
-    const markup = generateTaskMarkup(task.id, task.desc);
-    data.push(task);
+  if (task.desc === "") return;
+  const markup = generateTaskMarkup(task.id, task.desc);
+  data.push(task);
 
-    if (UI.todoItemsContainer.firstElementChild?.className === "message")
-        UI.todoItemsContainer.innerHTML = "";
+  if (UI.todoItemsContainer.firstElementChild?.className === "message")
+    UI.todoItemsContainer.innerHTML = "";
 
-    UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
-    UI.todoInput.value = "";
-    storeTasks();
-    displayTasksCount();
+  UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
+  UI.todoInput.value = "";
+  storeTasks();
+  displayTasksCount();
 });
 
 UI.completedBtn.addEventListener("click", (ev) => {
-    let completedTasks = data.filter((task) => {
-        return task.isCompleted === "true";
-    });
+  let completedTasks = data.filter((task) => {
+    return task.isCompleted === "true";
+  });
 
-    UI.filterBtns.forEach((btn) => {
-        btn.classList.remove("active");
-    });
+  UI.filterBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-    ev.target.classList.add("active");
-    UI.todoItemsContainer.innerHTML = "";
-    if (completedTasks.length === 0) {
-        UI.todoItemsContainer.innerHTML = `<p class="message">Tasks list is empty!</p>`;
-    }
-    completedTasks.forEach((task) => {
-        const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
-        UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
-    });
+  ev.target.classList.add("active");
+  UI.todoItemsContainer.innerHTML = "";
+  if (completedTasks.length === 0) {
+    UI.todoItemsContainer.innerHTML = `<li class="message">Tasks list is empty!</li>`;
+  }
+  completedTasks.forEach((task) => {
+    const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
+    UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
+  });
 });
 
 UI.activeBtn.addEventListener("click", (ev) => {
-    let incompletedTasks = data.filter((task) => {
-        return task.isCompleted === "false";
-    });
+  let incompletedTasks = data.filter((task) => {
+    return task.isCompleted === "false";
+  });
 
-    UI.filterBtns.forEach((btn) => {
-        btn.classList.remove("active");
-    });
+  UI.filterBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-    ev.target.classList.add("active");
+  ev.target.classList.add("active");
 
-    UI.todoItemsContainer.innerHTML = "";
-    if (incompletedTasks.length === 0) {
-        UI.todoItemsContainer.innerHTML = `<p class="message">Tasks list is empty!</p>`;
-    }
-    incompletedTasks.forEach((task) => {
-        const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
-        UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
-    });
+  UI.todoItemsContainer.innerHTML = "";
+  if (incompletedTasks.length === 0) {
+    UI.todoItemsContainer.innerHTML = `<li class="message">Tasks list is empty!</li>`;
+  }
+  incompletedTasks.forEach((task) => {
+    const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
+    UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
+  });
 });
 
 UI.allBtn.addEventListener("click", (ev) => {
-    UI.filterBtns.forEach((btn) => {
-        btn.classList.remove("active");
-    });
+  UI.filterBtns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-    ev.target.classList.add("active");
-    UI.todoItemsContainer.innerHTML = "";
+  ev.target.classList.add("active");
+  UI.todoItemsContainer.innerHTML = "";
 
-    if (data.length === 0) {
-        UI.todoItemsContainer.innerHTML = `<p class="message">Tasks list is empty!</p>`;
-    }
-    data.forEach((task) => {
-        const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
-        UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
-    });
+  if (data.length === 0) {
+    UI.todoItemsContainer.innerHTML = `<li class="message">Tasks list is empty!</li>`;
+  }
+  data.forEach((task) => {
+    const markup = generateTaskMarkup(task.id, task.desc, task.isCompleted);
+    UI.todoItemsContainer.insertAdjacentHTML("beforeend", markup);
+  });
 });
 
 UI.clearCompletedBtn.addEventListener("click", () => {
-    const completedTasks = data.filter((task) => task.isCompleted === "true");
+  const completedTasks = data.filter((task) => task.isCompleted === "true");
 
-    completedTasks.forEach((completedTask) => {
-        UI.todoItemsContainer.removeChild(
-            document.getElementById(completedTask.id)
-        );
-    });
+  completedTasks.forEach((completedTask) => {
+    UI.todoItemsContainer.removeChild(
+      document.getElementById(completedTask.id)
+    );
+  });
 
-    if (!UI.todoItemsContainer.children.length)
-        UI.todoItemsContainer.innerHTML = `<p class="message">Tasks list is empty!</p>`;
+  if (!UI.todoItemsContainer.children.length)
+    UI.todoItemsContainer.innerHTML = `<li class="message">Tasks list is empty!</li>`;
 
-    data = data.filter((task) => task.isCompleted === "false");
-    storeTasks();
+  data = data.filter((task) => task.isCompleted === "false");
+  storeTasks();
 });
 
 UI.todoItemsContainer.addEventListener("click", (ev) => {
-    const checkBox = "todo-item__checkbox";
-    const taskDelBtn = "todo-item__remove-btn";
-    let clickedEl =
-        ev.target.closest(`.${checkBox}`) ||
-        ev.target.closest(`.${taskDelBtn}`);
+  const checkBox = "todo-item__checkbox";
+  const taskDelBtn = "todo-item__remove-btn";
+  let clickedEl =
+    ev.target.closest(`.${checkBox}`) || ev.target.closest(`.${taskDelBtn}`);
 
-    if (!clickedEl) return;
-    const taskClicked = clickedEl.closest(".todo-item");
-    if (clickedEl.classList.contains(checkBox)) {
-        if (!clickedEl.hasAttribute("checked")) {
-            taskClicked.classList.add("done");
-            taskClicked.dataset.complete = "true";
-            clickedEl.setAttribute("checked", "");
+  if (!clickedEl) return;
+  const taskClicked = clickedEl.closest(".todo-item");
+  if (clickedEl.classList.contains(checkBox)) {
+    if (!clickedEl.hasAttribute("checked")) {
+      taskClicked.classList.add("done");
+      taskClicked.dataset.complete = "true";
+      clickedEl.setAttribute("checked", "");
 
-            data = data.map((task) => {
-                if (task.id === Number(taskClicked.id)) {
-                    return {
-                        id: task.id,
-                        desc: task.desc,
-                        isCompleted: "true",
-                    };
-                } else return task;
-            });
-        } else {
-            taskClicked.classList.remove("done");
-            taskClicked.dataset.complete = "false";
-            clickedEl.removeAttribute("checked");
-
-            data = data.map((task) => {
-                if (task.id === Number(taskClicked.id)) {
-                    return {
-                        id: task.id,
-                        desc: task.desc,
-                        isCompleted: "false",
-                    };
-                } else return task;
-            });
-        }
+      data = data.map((task) => {
+        if (task.id === Number(taskClicked.id)) {
+          return {
+            id: task.id,
+            desc: task.desc,
+            isCompleted: "true",
+          };
+        } else return task;
+      });
     } else {
-        taskClicked.parentElement.removeChild(taskClicked);
+      taskClicked.classList.remove("done");
+      taskClicked.dataset.complete = "false";
+      clickedEl.removeAttribute("checked");
 
-        if (!UI.todoItemsContainer.children.length)
-            UI.todoItemsContainer.innerHTML = `<p class="message">Tasks list is empty!</p>`;
-
-        data = data.filter((task) => task.id !== Number(taskClicked.id));
+      data = data.map((task) => {
+        if (task.id === Number(taskClicked.id)) {
+          return {
+            id: task.id,
+            desc: task.desc,
+            isCompleted: "false",
+          };
+        } else return task;
+      });
     }
+  } else {
+    taskClicked.parentElement.removeChild(taskClicked);
 
-    storeTasks();
-    displayTasksCount();
+    if (!UI.todoItemsContainer.children.length)
+      UI.todoItemsContainer.innerHTML = `<li class="message">Tasks list is empty!</li>`;
+
+    data = data.filter((task) => task.id !== Number(taskClicked.id));
+  }
+
+  storeTasks();
+  displayTasksCount();
 });
